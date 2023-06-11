f14b4a57e133964d0c80d37c651884b3b1605c76
Roland Chelwing <jolleee@hotmail.com>
2023-06-11 09:25:52 +0200

    Use main branch in release config

--- release.config.js
+++ release.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  branches: ["release"],
+  branches: ["main"],
   plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
 }
