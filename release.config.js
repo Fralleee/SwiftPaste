cac4fb4e25bdddaeb44b9b2d311181de868f102a
Roland Chelwing <jolleee@hotmail.com>
2023-06-11 09:39:58 +0200

    Configure release branches to be publish tags

--- release.config.js
+++ release.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  branches: ["refs/tags/publish/*"],
+  branches: ["^refs/tags/publish/\\d{4}-\\d{2}-\\d{2}\\.\\d+$"],
   plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
 }
