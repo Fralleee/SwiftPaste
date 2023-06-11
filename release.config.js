06f2aa57444b9e041e3b56a36453418c349f0be9
Roland Chelwing <jolleee@hotmail.com>
2023-06-11 09:03:48 +0200

    Update workflows

---
+++ release.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  branches: ["release"],
+  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
+}
---
+++ scripts/updateManifestVersion.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+const path = require("path")
+
+const [version] = process.argv.slice(2)
+const manifestPath = path.join(__dirname, "dist", "manifest.json")
+
+if (!version) {
+  console.error("Version number is required")
+  process.exit(1)
+}
+
+fs.readFile(manifestPath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`Error reading file from disk: ${err}`)
+    process.exit(1)
+  }
+
+  const manifest = JSON.parse(data)
+  manifest.version = version
+
+  fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), err => {
+    if (err) {
+      console.error(`Error writing file on disk: ${err}`)
+    }
+  })
+})
