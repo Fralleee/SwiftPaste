f2f47bbe87e1707e5f0edfd875813628779d702f
Roland Chelwing <jolleee@hotmail.com>
2023-06-11 09:54:02 +0200

    Test

--- release.config.js
+++ release.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  branches: ["main"],
+  branches: ["refs/tags/publish/2023-06-11.17", "refs/tags/publish/2023-06-11.18"],
   plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
 }
